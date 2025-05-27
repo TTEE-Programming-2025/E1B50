d1e6a0641d0b43089e1e21c9855e1d2011c86b7c
E1B50 <u11302250@o365.ttu.edu.tw>
2025-05-27 17:02:01 +0800

    sixth commit abcde全做完

--- P4E1B50.c
+++ P4E1B50.c
@@ -113,11 +113,54 @@ void b(){
 }
 
 
+//c
+void c(){
+	int i=0;
+	char target[20];
+	gets(target);
+	for(i=0;i<n;i++)
+	{
+	if(strcmp(target,student[i].name)==0)
+	{
+	student[i].avg=(student[i].math+student[i].phy+student[i].eng)/3.0; //計算平均
+    printf("姓名:%s 學號:%d 數學:%d 物理:%d 英文: %d 平均:%.1f\n",student[i].name,student[i].id,student[i].math,student[i].phy,student[i].eng,student[i].avg);
+    printf("\n");
+    }
+}
+	if(strcmp(target,student[i].name)!=0)
+	printf("查無此人");
+system("pause");
+}
 	
+//d
+void d(){
+	int i,j;
+	struct data x;
+	for(i=0;i<n;i++)
+	{
+	student[i].avg=(student[i].math+student[i].phy+student[i].eng)/3.0; //計算平均
+	}
+	for(i = 0; i < n - 1; i++) {
+        for(j = 0; j < n - 1 ; j++) {
+            if(student[j].avg < student[j+1].avg) {
+                x = student[j];
+                student[j] = student[j+1];
+                student[j+1] = x;
+            }
+        }
+    }
+	for(i=0;i<n;i++)
+	{
+	printf("姓名:%s 學號:%d 數學:%d 物理:%d 英文: %d 平均:%.1f 排名%d\n",student[i].name,student[i].id,student[i].math,student[i].phy,student[i].eng,student[i].avg,i+1);
+    printf("\n");
+	}
+	system("pause");
+}	
 int main(void){
- char password[4];
- int m=15,i,j,count;
- printf("        E1B50王楊馥華\n");
+char c2;
+char password[4];
+int m=15,i,j,count;
+printf("        E1B50王楊馥華\n");
 for(i=0;i<m;i++){
 	for(j=0;j<m-i-1;j++){
 		printf(" ");
@@ -134,6 +177,7 @@ for(i=0;i<m;i++){
 	printf("//.,,.//,//|||||||\\\\\\,//.//,//,/,\n");
 	printf("請輸入密碼:");
 	
+
 do{
 	int i;
 	for(i=0;i<4;i++){
@@ -157,7 +201,7 @@ do{
 }while(count<3);
 
 	system("cls");
-while(1){
+	while(1){
 	printf("------------[Grade System]----------\n");
     printf("| a. Enter student grades          |\n");
     printf("| b. Display student grades        |\n");
@@ -166,31 +210,42 @@ while(1){
     printf("| e. Exit system                   |\n");
     printf("------------------------------------\n");
     
-    
+
     char f;
 	printf("請輸入a-e:");
 	scanf("%c",&f); 
 	fflush(stdin); //防止緩衝區殘留
 	system("cls");//清除畫面
 	switch(f){
-		case 'a':
+		case 'a':case'A':
 			a();
 			break;
-		case 'b':
+		case 'b':case'B':
 			b();
 			break;
-	/*	case 'c':
+	    case 'c':case'C':
 			c();
 			break;
-		case 'd':
+		case 'd':case'D':
 			d();
 			break;
-		case 'e':
-			e();
-			break;
-			*/
+		case 'e':case'E':
+                do {
+                    printf("Continue? (y/n): ");
+                    fflush(stdin);
+                    c2 = getchar();
+                    if (c2 == 'y'||c2 == 'Y') 
+					break;
+                    if (c2=='n'||c2=='N') 
+					{ 
+					printf("Goodbye!\n");
+					return 0; 
+					}
+                    printf("Invalid input. ");
+                } while (1);
+                break;
+        }
 	}
-}
     return 0;
 }
 
